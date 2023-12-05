ac371de6c50dfd4b3e6373e6ff2ae798679bd4ad
Sam Gammon <sam@elide.ventures>
2023-12-04 17:39:18 -0800

    chore: centrally gather action env
    Signed-off-by: Sam Gammon <sam@elide.ventures>

--- src/config.ts
+++ src/config.ts
@@ -18,6 +18,36 @@ export const BUILDLESS_AGENT_ENDPOINT = 'https://agent.less.build'
 // Azure CDN cache.
 export const BUILDLESS_AZR_ENDPOINT = 'https://azr.less.build'
 
+// Actions Runtime-provided environment material.
+export const actionEnv = {
+  idTokenRequestToken: process.env.ACTIONS_ID_TOKEN_REQUEST_TOKEN || null,
+  idTokenRequestUrl: process.env.ACTIONS_ID_TOKEN_REQUEST_URL || null,
+  runtimeToken: process.env.ACTIONS_RUNTIME_TOKEN || null,
+  runtimeTokenUrl: process.env.ACTIONS_RUNTIME_URL || null,
+  githubEnvFile: process.env.GITHUB_ENV || null,
+  githubEventName: process.env.GITHUB_EVENT_NAME || null,
+  githubEventPath: process.env.GITHUB_EVENT_PATH || null,
+  githubJobName: process.env.GITHUB_JOB || null,
+  githubActionRef: process.env.GITHUB_ACTION_REF || null,
+  githubRunId: process.env.GITHUB_RUN_ID || null,
+  githubRunNumber: process.env.GITHUB_RUN_NUMBER || null,
+  githubSha: process.env.GITHUB_SHA || null,
+  githubWorkflow: process.env.GITHUB_WORKFLOW || null,
+  githubWorkflowRef: process.env.GITHUB_WORKFLOW_REF || null,
+  githubWorkflowSha: process.env.GITHUB_WORKFLOW_SHA || null,
+  invocationId: process.env.INVOCATION_ID || null,
+  imageOs: process.env.ImageOS || null,
+  imageVersion: process.env.ImageVersion || null,
+  runnerArch: process.env.RUNNER_ARCH || null,
+  runnerDebug: process.env.RUNNER_DEBUG || null,
+  runnerEnvironment: process.env.RUNNER_ENVIRONMENT || null,
+  runnerName: process.env.RUNNER_NAME || null,
+  runnerOs: process.env.RUNNER_OS || null,
+  runnerTemp: process.env.RUNNER_TEMP || null,
+  runnerUser: process.env.RUNNER_USER || null,
+  runnerWorkspace: process.env.RUNNER_WORKSPACE || null,
+}
+
 // Transport modes.
 export enum RpcTransport {
   CONNECT = 'CONNECT',
