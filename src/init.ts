234fcadc9f33e3b8acd433a46a22af01b40613cf
Sam Gammon <sam@elide.ventures>
2023-12-04 20:50:25 -0800

    fix: drop node profiler, update dependencies
    Signed-off-by: Sam Gammon <sam@elide.ventures>

--- src/init.ts
+++ src/init.ts
@@ -1,9 +1,7 @@
 import * as Sentry from '@sentry/node'
-import { ProfilingIntegration } from '@sentry/profiling-node'
 
 Sentry.init({
   dsn: 'https://5c41fe474b8d80994261103ab94b560d@o4504604187951104.ingest.sentry.io/4506341130698752',
-  integrations: [new ProfilingIntegration()],
   tracesSampleRate: 1.0,
   profilesSampleRate: 1.0
 })
