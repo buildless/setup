9a9d261af93a53921007f1e9eab45d3cdb39a0d7
Sam Gammon <sam@elide.ventures>
2023-12-05 01:17:09 -0800

    test: cleanup and centralize test utils
    Signed-off-by: Sam Gammon <sam@elide.ventures>

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,17 +1,21 @@
-/**
- * Unit tests for the action's entrypoint, src/index.ts
- */
-
 import * as main from '../src/main'
+import { setupCoreMocks } from './utils'
 
-const runMock = jest.spyOn(main, 'entry').mockImplementation()
-const cleanupMock = jest.spyOn(main, 'cleanup').mockImplementation()
+const { clearMocks, resetState } = setupCoreMocks()
+const runMock = jest.spyOn(main, 'entry').mockImplementation(() => Promise.resolve())
+const cleanupMock = jest.spyOn(main, 'cleanup').mockImplementation(() => Promise.resolve())
 
 describe('index', () => {
+  afterEach(() => {
+    clearMocks()
+  })
+  afterAll(async () => {
+    await resetState()
+  })
+
   it('calls entrypoint when imported', async () => {
     // eslint-disable-next-line @typescript-eslint/no-require-imports
     require('../src/index')
-
     expect(runMock).toHaveBeenCalled()
   })
 })
@@ -20,7 +24,6 @@ describe('cleanup', () => {
   it('calls entrypoint when imported', async () => {
     // eslint-disable-next-line @typescript-eslint/no-require-imports
     require('../src/cleanup')
-
     expect(cleanupMock).toHaveBeenCalled()
   })
 })
